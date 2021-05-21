43b4b42dbd66cfe241dbbf13dd8e0ecd976c8568
orange-0720 <orange0720@hotmail.com>
2021-05-21 19:38:23 +0800

    RWD變更

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ var ham_btn = document.getElementById("hamburger");
 var ham_under = document.getElementById("hamburger_under_line");
 var ham_middle = document.getElementById("hamburger_middle_line");
 var ham_top = document.getElementById("hamburger_top_line");
-var header = document.getElementsByTagName("header")[0];
+var header_nav = document.getElementById("header_nav");
 
 
 ham_btn.addEventListener("click", function(){
@@ -10,5 +10,5 @@ ham_btn.addEventListener("click", function(){
     ham_top.classList.toggle("up_donw");
     ham_middle.classList.toggle("middle");
     ham_under.classList.toggle("donw_up");
-    header.classList.toggle("header");
+    header_nav.classList.toggle("nav_open");
 })
