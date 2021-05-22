1a01a1e0aef0d354169594bcc8d75409d8c44df2
orange-0720 <orange0720@hotmail.com>
2021-05-22 14:23:22 +0800

    RWD新增 & JS新增

--- js/header.js
+++ js/header.js
@@ -12,3 +12,35 @@ ham_btn.addEventListener("click", function(){
     ham_under.classList.toggle("donw_up");
     header_nav.classList.toggle("nav_open");
 })
+
+var footer_nav_list1 = document.getElementsByClassName("footer_nav_list")[0];
+var footer_nav_list2 = document.getElementsByClassName("footer_nav_list")[1];
+var footer_nav_list3 = document.getElementsByClassName("footer_nav_list")[2];
+var footer_brand_all = document.getElementsByClassName("footer_brand_all");
+var footer_product_all = document.getElementsByClassName("footer_product_all");
+var footer_know_all = document.getElementsByClassName("footer_know_all");
+
+footer_nav_list1.onclick = function(){
+    document.getElementsByClassName("footer_intro")[0].classList.toggle("slide_down");
+    footer_brand_all[0].classList.toggle("footer_nav_open");
+    footer_brand_all[1].classList.toggle("footer_nav_open");
+    footer_brand_all[2].classList.toggle("footer_nav_open");
+    footer_brand_all[3].classList.toggle("footer_nav_open");
+}
+
+footer_nav_list2.onclick = function(){
+    document.getElementsByClassName("footer_product")[0].classList.toggle("slide_down");
+    footer_product_all[0].classList.toggle("footer_nav_open");
+    footer_product_all[1].classList.toggle("footer_nav_open");
+    footer_product_all[2].classList.toggle("footer_nav_open");
+    footer_product_all[3].classList.toggle("footer_nav_open");
+}
+
+footer_nav_list3.onclick = function(){
+    document.getElementsByClassName("footer_knowledge")[0].classList.toggle("slide_down");
+    footer_know_all[0].classList.toggle("footer_nav_open");
+    footer_know_all[1].classList.toggle("footer_nav_open");
+    footer_know_all[2].classList.toggle("footer_nav_open");
+    footer_know_all[3].classList.toggle("footer_nav_open");
+}
+
