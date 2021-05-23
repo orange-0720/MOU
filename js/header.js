e23647d6d7a375c33ce5349d9b7c92be162a61f2
orange-0720 <orange0720@hotmail.com>
2021-05-23 16:11:06 +0800

    QA新增RWD

--- js/header.js
+++ js/header.js
@@ -8,7 +8,7 @@ var header_nav = document.getElementById("header_nav");
 ham_btn.addEventListener("click", function(){
     // alert("hello");
     ham_top.classList.toggle("up_donw");
-    ham_middle.classList.toggle("middle");
+    ham_middle.classList.toggle("middle_line");
     ham_under.classList.toggle("donw_up");
     header_nav.classList.toggle("nav_open");
 })
