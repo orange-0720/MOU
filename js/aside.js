d6c51744e13059e17c6b5dbd7fb00a1acbbf8f47
orange-0720 <orange0720@hotmail.com>
2021-06-09 09:40:10 +0800

    調整js及aside

--- js/aside.js
+++ js/aside.js
@@ -5,3 +5,13 @@ aside_btn.onclick = function(){
     aside_btn.classList.toggle("aside-open");
     aside.classList.toggle("aside-on")
 }
+
+document.addEventListener("click", function(e){
+    if(e.target.closest("aside.aside")){
+        console.log("點擊在aside側邊攔上")
+    }else{
+        if(aside.classList.contains("aside-on")){
+            aside.classList.remove("aside-on");
+        }
+    }
+})
--- js/header.js
+++ js/header.js
@@ -90,9 +90,9 @@ background_el.addEventListener("click", function(){
 
 document.addEventListener("click", function(e){
     if(window.innerWidth <= 767){
-        let showup_el = e.target.closest("div").querySelector("a.showup_el");
         if(e.target.classList.contains("prevent")){
             e.preventDefault();
+            let showup_el = e.target.closest("div").querySelector("a.showup_el");
             let slide_el = e.target.nextElementSibling;
             showup_el.classList.toggle("showup_add");
             slide_el.classList.toggle("prevent_open");
